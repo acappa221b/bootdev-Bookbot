0b8dbbbc8d7b3fb22658a94f4b450057138f3df0
acappa221b <alexandrecappa12@gmail.com>
2026-01-05 15:56:18 -0300

    ok

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ def main():
     # books to read
     book_name = "books/frankenstein.txt"
     get_book_text(book_name)
+    num_words(get_book_text(book_name))
+
 
 
 
 def get_book_text(book_name):
     with open(book_name) as f:
         book_content = f.read()
-    return print(book_content)
+    return book_content
 
-
+def num_words(book):
+    words = book.split()
+    return print (f"Found {len(words)} total words")
 
 
 
