397fa4bc744e3ad3c5eb6bf8800a883ee1dbf6a1
acappa221b <alexandrecappa12@gmail.com>
2026-01-05 18:17:52 -0300

    Finished bookbot from Boot.dev

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-        book_name = sys.argv[1]
+    book_name = sys.argv[1]
     book = get_book_text(book_name)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_name}...")
